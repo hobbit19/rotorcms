0faa67941b0eebb718f4b874667867e85254042a
visavi <visavi.net@mail.ru>
2013-03-04 14:11:37 +0400

    Русификация списка ошибок валидации

---
+++ fuel/app/lang/ru/validation.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+	'required'        => 'Поле :label является обязательным и должно содержать значение',
+	'min_length'      => 'Поле :label короче :param:1 зн.',
+	'max_length'      => 'Поле :label слишком длинное, максимум :param:1 зн.',
+	'exact_length'    => 'Поле :label должно быть равным длине :param:1',
+	'match_value'     => 'Поле :label должно содержать значение :param:1',
+	'match_pattern'   => 'Поле :label не соответствует :param:1',
+	'match_field'     => 'Поле :label должно совпадать с Полем :param:1',
+	'valid_email'     => 'Поле :label должно содержать корректный email адрес',
+	'valid_emails'    => 'Поле :label должно содержать корректные email адреса',
+	'valid_url'       => 'Поле :label должно содержать корректный URL',
+	'valid_ip'        => 'Поле :label должно содержать корректный IP',
+	'numeric_min'     => 'Минимальное числовое значение поля :label = :param:1',
+	'numeric_max'     => 'Максимальное числовое значение поля :label = :param:1',
+	'numeric_between' => 'Поле :label должно содержать числовое значение между :param:1 и :param:2',
+	'valid_string'    => 'Строка :rule(:param:1) не верна :label',
+	'required_with'   => 'Поле :label является обязательным, если :param:1 содержит значение.',
+);
