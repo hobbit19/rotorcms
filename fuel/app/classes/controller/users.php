9551111a19790fbf05ebf2a225663384c7063ba3
visavi <visavi.net@mail.ru>
2013-05-17 16:16:41 +0400

    closed #16 Исправлено восстановление пароля

--- fuel/app/classes/controller/users.php
+++ fuel/app/classes/controller/users.php
@@ -213,6 +213,7 @@ class Controller_Users extends \Controller_Base
 		{
 			try
 			{
+				\Cartalyst\Sentry\Users\Eloquent\User::setLoginAttribute('email');
 				$user = Sentry::getUserProvider()->findByLogin(\Input::post('email'));
 
 				$resetCode = $user->getResetPasswordCode();
