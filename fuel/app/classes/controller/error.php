6e5d121a09905c61f60d4734c49d940320ef4aae
visavi <visavi.net@mail.ru>
2013-05-24 13:26:17 +0400

    Изменил страницу 404

--- fuel/app/classes/controller/error.php
+++ fuel/app/classes/controller/error.php
@@ -4,6 +4,13 @@ class Controller_Error extends \Controller_Base
 {
 	public function action_404()
 	{
-		return Response::forge(ViewModel::forge('error/404'), 404);
+		$messages = array('Aw, crap!', 'Bloody Hell!', 'Uh Oh!', 'Nope, not here.', 'Huh?');
+		$title = $messages[array_rand($messages)];
+
+		\Breadcrumb::remove(1);
+		$this->template->fullpage = true;
+		$this->template->title = $title;
+		$this->template->subtitle = 'We can\'t find that!';
+		$this->template->content = View::forge('error/404', array('title' => $title));
 	}
 }
