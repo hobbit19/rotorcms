2b8e1a1178c3637dfbfee29f1c51c9e503b989f1
Alexandr Sytnyk <syzenqq@gmail.com>
2013-07-02 03:44:18 +0300

    contact language

--- fuel/app/classes/controller/contact.php
+++ fuel/app/classes/controller/contact.php
@@ -2,10 +2,15 @@
 
 class Controller_Contact extends Controller_Base
 {
+	public function before()
+	{
+		parent::before();
+		\Lang::load('contact');
+	}
 
 	public function action_index()
 	{
-		$this->template->title = 'Contact';
+		$this->template->title = \Lang::get('index.page_title');
 		$this->template->content = View::forge('contact/index');
 	}
 
---
+++ fuel/app/lang/ru/contact.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+	'breadcrumb' => array(
+		'contact' => 'Контакты',
+	),
+	'index' => array(
+		'page_title' => 'Обратная связь',
+	),
+);
