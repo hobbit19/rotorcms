c7ee8962a8c92105a10e86248499b5f953a859ea
visavi <visavi.net@mail.ru>
2013-08-13 22:58:23 +0400

    Поправил совместимость с bootstrap 3.0

--- fuel/app/views/admin/dashboard.php
+++ fuel/app/views/admin/dashboard.php
@@ -1,41 +1,50 @@
-<div class="row-fluid">
-	<div class="span4">
-		<h4>Последниe пользователи</h4>
+<div class="row">
+	<div class="col-lg-4">
+		<div class="panel">
+			<div class="panel-heading">
+				<h4 class="panel-title"><?=Html::anchor('admin/users', 'Последниe пользователи')?></h4>
+			</div>
+		<?if ($users):?>
+		<?foreach ($users as $user):?>
+			<div>
+				<?=Html::img('http://www.gravatar.com/avatar/'.md5($user->email).'?size=16&amp;d=mm', array('alt' => $user->username))?>
+				<?=Html::anchor('users/'.$user->id, $user->username)?>
+			</div>
+		<?endforeach?>
 
-	<?if ($users):?>
-	<?foreach ($users as $user):?>
-		<div>
-			<?=Html::img('http://www.gravatar.com/avatar/'.md5($user->email).'?size=16&amp;d=mm', array('alt' => $user->username))?>
-			<?=Html::anchor('users/'.$user->id, $user->username)?>
+			<p>Всего пользователей: <?=$count_users?></p>
+		<?else:?>
+			<p>No users</p>
+		<?endif?>
 		</div>
-	<?endforeach?>
-
-		<p>Всего пользователей: <?=$count_users?></p>
-	<?else:?>
-		<p>No users</p>
-	<?endif?>
 	</div>
 
-
-	<div class="span4">
-		<h4>Последниe сообщения</h4>
-
-		<p>No messages</p>
+	<div class="col-lg-4">
+		<div class="panel">
+			<div class="panel-heading">
+				<h4 class="panel-title">Последниe сообщения</h4>
+			</div>
+			<p>No messages</p>
+		</div>
 	</div>
 
-	<div class="span4">
-		<h4><?=Html::anchor('admin/news', 'Последниe новости')?></h4>
-	<?if ($news):?>
-	<?foreach ($news as $news_item):?>
-		<div>
-			<i class="icon-edit"></i> <?=Html::anchor('news/view/'.$news_item->id, $news_item->title)?>
-		</div>
-	<?endforeach?>
+	<div class="col-lg-4">
+		<div class="panel">
+			<div class="panel-heading">
+				<h4 class="panel-title"><?=Html::anchor('admin/news', 'Последниe новости')?></h4>
+			</div>
+		<?if ($news):?>
+		<?foreach ($news as $news_item):?>
+			<div>
+				<i class="icon-edit"></i> <?=Html::anchor('news/view/'.$news_item->id, $news_item->title)?>
+			</div>
+		<?endforeach?>
 
-		<p>Всего новостей: <?=$count_news?></p>
-	<?else:?>
-		<p>No themes</p>
-	<?endif?>
+			<p>Всего новостей: <?=$count_news?></p>
+		<?else:?>
+			<p>No themes</p>
+		<?endif?>
+		</div>
 	</div>
 </div>
 
--- fuel/modules/guestbook/views/_form.php
+++ fuel/modules/guestbook/views/_form.php
@@ -2,15 +2,17 @@
 	<fieldset>
 		<legend><?= __('create.new_message') ?></legend>
 
-		<div class="control-group">
-			<?=\Form::label(__('create.message'), 'text', array('class' => 'control-label'))?>
-			<div class="controls">
-				<?=\Form::textarea('text', \Input::post('text'), array('class' => 'span8', 'rows' => 5))?>
+		<div class="form-group">
+			<?=\Form::label(__('create.message'), 'text', array('class' => 'col-lg-2 control-label'))?>
+			<div class="col-lg-10">
+				<?=\Form::textarea('text', \Input::post('text'), array('class' => 'form-control', 'rows' => 5))?>
 			</div>
 		</div>
 
-		<div class="form-actions">
-			<?=\Form::submit('submit', __('create.submit'), array('class' => 'btn btn-primary'))?>
+		<div class="form-group">
+			<div class="col-lg-offset-2 col-lg-10">
+				<?=\Form::submit('submit', __('create.submit'), array('class' => 'btn btn-primary'))?>
+			</div>
 		</div>
 
 	</fieldset>
